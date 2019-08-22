658db15897b9ac11c8188567298a652bf761583b
jaybenaim <benaimjacob@gmail.com>
2019-08-22 12:42:21 -0400

    api broken

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -1,3 +1,66 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const rootButton = $('.root')
+    const ul = $('#data')
+
+    rootButton.click(() => { 
+        // runtime 14ms
+        requestRoot(); 
+    }) 
+
+    // rootButton.click(() => { 
+    //     let rootRequest = axios.get('http://intro-ajax-api.herokuapp.com/')
+
+    //     rootRequest.then(response => { 
+    //         console.log(response)
+            
+    //         const data = JSON.stringify(response) 
+    //         ul.append(`<li> ${data}</li>`)
+    //         // rootRequest.then(response => { 
+    //         //     for(let i =0; i <data.length; i++){
+    //         //         ul.append(`<li> ${data}</li> `)
+    //         //     }
+                
+    //         return data
+        
+    //         })
+    //     });
+         
+        // rootRequest.then(response => { 
+        // console.log(response)
+        // rootRequest = JSON.stringify(response)
+        // ul.append(rootRequest)
+        // })
+        // .done(responseData => { 
+        //     for (let i = 0; i < response.data.length; i++){ 
+        //         ul.append(i)
+            
+        //     }
+    // }) 
+
+
+     
+    async function requestRoot() {
+        // runtime 14ms 
+        try { 
+            let rootRequest = await axios.get('http://intro-ajax-api.herokuapp.com/')
+            // const rootRequest = await axios.get('https://bb-election-api.herokuapp.com/')
+            console.log(rootRequest)            
+            rootRequest = JSON.stringify(rootRequest)
+            rootButton.append(rootRequest)
+
+        } catch (error){ 
+            console.log(error)
+        }
+    }
 
 });
+ 
+// rootButton.click(() => { 
+    // runtime 18ms 
+    // const rootRequest = axios.get('http://intro-ajax-api.herokuapp.com/')
+    // rootRequest.then(response => { 
+    //     console.log(response)
+    //     rootRequest = JSON.stringify(rootRequest)
+    //     rootButton.append(rootRequest)
+    //     }) 
+    // }) 
