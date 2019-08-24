2308c8a1356096572c2c9bd8fa3b6eeda8b24ca8
jaybenaim <benaimjacob@gmail.com>
2019-08-24 11:40:10 -0400

    display all names correctly

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -8,57 +8,78 @@ document.addEventListener("DOMContentLoaded", function() {
 
     rootButton.addEventListener('click', () => { 
         requestRoot(); 
-        console.log(form)
-
     }) 
-    submitButton.addEventListener('click', event => { 
-        event.preventDefault(); 
-        console.log('ss')
-        alert('hello')
-    })
+    // submitButton.addEventListener('click', event => { 
+    //     event.preventDefault(); 
+    //     console.log(name)
+    //     console.log('ss')
+    //     alert('hello')
+    // })
 
 });
 
-
-
 async function requestRoot() {
     const ul = document.querySelector('#data')
-    const li = document.createElement('li')
-    const form = document.createElement('form')
-    const hiddenInput = document.createElement('input')
-    const submitButton = document.createElement('input')
+  
+    const findLi = document.querySelectorAll('li')
+
     
     try { 
         let rootRequest = await axios.get('https://bb-election-api.herokuapp.com/')
-        console.log(rootRequest.data)            
+        // console.log(rootRequest.data)            
         rootRequest = rootRequest.data.candidates
-        rootRequest.forEach(candidate => { 
-            name = candidate.name
-            votes = candidate.votes
+       
+            console.log()
+            const names = [] 
+        for (let i = 0; i < rootRequest.length; i++){     
+            const li = document.createElement('li')
+            const form = document.createElement('form')
+            const hiddenInput = document.createElement('input')
+            const submitButton = document.createElement('input')    
             ul.append(li)
             ul.append(form)
             form.append(hiddenInput)
             form.append(submitButton)
-            li.innerHTML = `Name: ${name} | Votes: ${votes} `
-
+            
+            li.textContent = `Name: ${rootRequest[i].name} | Votes: ${rootRequest[i].votes} `
+           
             form.setAttribute("class", "form")
             form.setAttribute("action", "https://bb-election-api.herokuapp.com/vote")
             form.setAttribute("method", "POST")
 
             hiddenInput.setAttribute("type", "hidden") 
-            hiddenInput.setAttribute("name", "id") 
-            hiddenInput.setAttribute("value", candidate.id) 
-
+            hiddenInput.setAttribute("name", "name") 
+            hiddenInput.setAttribute("value", rootRequest[i].name) 
             submitButton.setAttribute( "id", "submit-button") 
             submitButton.setAttribute( "type", "submit") 
-
-        })
+        }
+        
+        handleSubmit();
+         
     } catch (error){ 
         console.log(error)
     }
 }
 
+const handleSubmit = () => {
+ 
+    let form = document.querySelector('form')
 
+    form.addEventListener('submit', event => { 
+        event.preventDefault(); 
+        name = event.target.querySelector('input[type=hidden]').value
+            axios.post(form.action, { 
+            name: name
+        }, { headers: { 'X-Requested-With': 'XMLHttpRequest'}})
+        .then(response => { 
+            console.log(response.data)
+        })
+        .catch(error => { 
+            console.log(error)
+        })
+    })
+
+}
 
 // async function requestRoot() {
 //     const ul = $('#data')
@@ -72,7 +93,28 @@ async function requestRoot() {
 
 //             ul.append("<li> Name: " + name + " | Votes: " + votes + "<form id='form' action='https://bb-election-api.herokuapp.com/vote' method='POST'><input type='hidden' name='id' value='"+candidate.id+"'><button id='submit-button' type='submit' > Vote </button> <form></li>")
 //         })
+//         handleSubmit();
 //     } catch (error){ 
 //         console.log(error)
 //     }
 // }
+
+
+// const requestRoot = () =>  {
+//     const forms = document.querySelectorAll('form')
+
+//     forms.forEach(form => { 
+//         form.addEventListener('click', event => { 
+//             e.preventDefault(); 
+//             axios.get(link.href, { headers: { 'X-Requested-With':'XMLHttpRequest'}})
+//             .then(response => { 
+//                 let data = response.data 
+//                 console.log(data)
+//             })
+//             handleSubmit();
+             
+//         })
+//     })
+  
+// }
+
